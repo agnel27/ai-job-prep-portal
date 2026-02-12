c9f7bd5e1b1cb2fa6005e4b2fe01426fc9b5de52
agnel27 <agnelkingston.r@gmail.com>
2026-02-12 15:35:03 +0530

    Fix database path for Render

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config:
     SECRET_KEY = os.getenv("FLASK_SECRET_KEY", "dev_secret")
-    SQLALCHEMY_DATABASE_URI = "sqlite:///instance/database.db"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "database.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = "uploads"
+    UPLOAD_FOLDER = os.path.join(basedir, "uploads")
