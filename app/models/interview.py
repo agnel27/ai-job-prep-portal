0b09add087f627dfb7adf7275ad3b76a0d413dfc
agnel27 <agnelkingston.r@gmail.com>
2026-02-12 14:59:57 +0530

    Fixed chatbot import

--- app/models/interview.py
+++ app/models/interview.py
@@ -1,9 +1,7 @@
 from app import db
-from datetime import datetime
 
-class InterviewScore(db.Model):
+class InterviewResult(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer)
-    score = db.Column(db.Integer)
+    score = db.Column(db.Float)
     feedback = db.Column(db.Text)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
